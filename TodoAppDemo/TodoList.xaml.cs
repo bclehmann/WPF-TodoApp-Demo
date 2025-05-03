266493fae54ceda1e61949ecc51500881e7a7269
Benjamin Lehmann <bclehmann@yahoo.ca>
2025-05-03 11:56:19 -0600

    Move TodoList into own UserControl

---
+++ TodoAppDemo/TodoList.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using TodoAppDemo.ViewModels;
+
+namespace TodoAppDemo;
+
+public partial class TodoList : UserControl
+{
+    private TaskListViewModel TypedDataContext => DataContext as TaskListViewModel ?? throw new ArgumentNullException(nameof(DataContext));
+    public TodoList()
+    {
+        InitializeComponent();
+        DataContext = new ViewModels.TaskListViewModel();
+    }
+    
+    public static RoutedCommand AddEmptyTaskCommand = new RoutedCommand();
+
+    public void AddEmptyTaskExecuted(object sender, ExecutedRoutedEventArgs e)
+    {
+        TypedDataContext.AddTask(string.Empty);
+    }
+
+    private void TextBoxKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            e.Handled = true;
+            TypedDataContext.AddTask(string.Empty);
+        }
+    }
+}
